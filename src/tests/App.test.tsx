18bb80895f5eeeb5feac3b3cd0c3d44889837507
Noah <noahisaacshirey@gmail.com>
2019-11-20 00:50:28 -0500

    Fix app render test.

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from  'enzyme';
 import App from '../App';
+import { BrowserRouter } from 'react-router-dom';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>) , div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
