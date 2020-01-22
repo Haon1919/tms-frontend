1b1bd2237e0e4ebbbd581d432f4fcdb7af588bf3
Noah <noahisaacshirey@gmail.com>
2020-01-22 14:32:55 -0500

    Fix className, bug
    Fix bug where non required fields with validation could be submitted even if they failed the validation.

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -1,37 +1,30 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState, useMemo } from 'react';
+import { useHistory } from "react-router-dom";
 import styles from '../styles/CreateAccount.module.scss';
 import { FormInput } from '../components/FormInput';
 import DateFnsUtils from '@date-io/date-fns';
+import moment from 'moment';
 import {
     MuiPickersUtilsProvider,
     DatePicker
 } from '@material-ui/pickers';
-
-type FormData = {
-    focusedInput: string,
-    date: string,
-    firstName: string,
-    lastName: string,
-    email: string,
-    phone: string,
-    password: string
-}
-
-type ReducerParams = {
-    type: string,
-    formDataKey?: keyof FormData,
-    value: string
-}
+import { FormData, ReducerParams } from '../types/CreateAccountTypes';
 
 const initialFormData: FormData = {
     focusedInput: "",
-    date: new Date().toString(),
+    birthday: new Date().toString(),
     firstName: "",
     lastName: "",
     email: "",
     phone: "",
-    password: ""
+    password: "",
+    guardianEmail: ""
 }
+
+// eslint-disable-next-line
+const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// eslint-disable-next-line
+const phonePattern = /((\(\d{3}\)?)|(\d{3}))([\s-./]?)(\d{3})([\s-./]?)(\d{4})/;
 
 function createAccountReducer(state: FormData, action: ReducerParams) {
     switch (action.type) {
@@ -53,55 +46,156 @@ function createAccountReducer(state: FormData, action: ReducerParams) {
     }
 }
 
+function checkMinorStatus(birthday: string): boolean {
+    //Check to see if the user is above the age of 18
+    return moment(birthday).isAfter(moment().subtract(18, 'years'));
+}
+
 export const CreateAccount: React.FC = () => {
     const [formData, dispatch] = useReducer(createAccountReducer, initialFormData);
+    const [failingForms, setFailingForms] = useState<string[]>([]);
+
+    let history = useHistory();
+    const isDisabled = checkMinorStatus(formData.birthday) ? 
+        (formData.firstName === "" || formData.lastName === "" || formData.guardianEmail === "" || formData.password === "") || !!failingForms.length:
+        (formData.firstName === "" || formData.lastName === "" || formData.email === "" || formData.password === "") || !!failingForms.length;
+
+    let emailValidation = useMemo(() => {
+        return {
+            pattern: emailPattern,
+            failedMesg: "Please provide a valid email.",
+            changeInvalidFormsList: setFailingForms
+        }
+    }, []);
+
+    let guardianValidation = useMemo(() => {
+        return {
+            pattern: emailPattern,
+            failedMesg: "Please provide a valid email.",
+            changeInvalidFormsList: setFailingForms
+        }
+    }, []);
+
+    let phoneValidation = useMemo(() => {
+        return {
+            pattern: phonePattern,
+            failedMesg: "Please provide full phone number.",
+            changeInvalidFormsList: setFailingForms
+        }
+    }, []);
+
 
     const changeDate = (date: Date) => {
         const action: ReducerParams = {
             type: "UPDATEFORM",
-            formDataKey: "date",
+            formDataKey: "birthday",
             value: date.toString()
         };
         dispatch(action);
     }
 
+    const phoneNumberDispatch = (action: ReducerParams): void => {
+        if (action.type === "UPDATEFORM") {
+            let formattedNumber = "";
+            let lastChar = action.value.charAt(action.value.length - 1);
+
+            if (/[(,),\-,^]/.test(lastChar)) {
+                switch (lastChar) {
+                    case ("("):
+                        if (action.value.lastIndexOf(lastChar) !== 0) return;
+                        break;
+                    case (")"):
+                        if (action.value.lastIndexOf(lastChar) !== 4) return;
+                        break;
+                    case ("-"):
+                        let firstDashCheck = action.value.lastIndexOf(lastChar) !== 5 && action.value.length < 9;
+                        let secondDashCheck = action.value.lastIndexOf(lastChar) !== 9 && action.value.length > 9;
+                        if (firstDashCheck || secondDashCheck) return;
+                }
+            } else if (/(?=[^0-9])(?=[^\s])/.test(action.value.charAt(action.value.length - 1))) {
+                return
+            }
+
+            switch (action.value.length) {
+                case (3):
+                    if (formData.phone.length < action.value.length) {
+                        formattedNumber = `(${action.value}) `;
+                    } else {
+                        formattedNumber = action.value;
+                    }
+                    break;
+                case (9):
+                    if (formData.phone.length < action.value.length) {
+                        formattedNumber = `${action.value}-`;
+                    } else {
+                        formattedNumber = action.value;
+                    }
+                    break;
+                case (15):
+                    if (formData.phone.length < action.value.length) {
+                        formattedNumber = action.value.substring(0, action.value.length - 1);
+                    } else {
+                        formattedNumber = action.value;
+                    }
+                    break;
+                default:
+                    formattedNumber = action.value;
+            }
+            action.value = formattedNumber
+            dispatch(action);
+        } else if (action.type === "FOCUS") {
+            dispatch(action)
+        }
+    }
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        new Promise((resolve) => {
+            setTimeout(() => {
+                resolve();
+            }, 500)
+        }).then(() => {
+            history.push("/Dashboard")
+        })
+    }
+
     return (
         <section className={styles.create_account}>
             <h1>Create Account</h1>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <div className={styles.date_picker}>
                     <MuiPickersUtilsProvider utils={DateFnsUtils}>
                         <DatePicker
-                            onOpen={() => dispatch({type: "FOCUS", value: ""})}
+                            onOpen={() => dispatch({ type: "FOCUS", value: "" })}
                             views={["year"]}
                             label="Year"
-                            value={new Date(formData.date)}
+                            value={new Date(formData.birthday)}
                             onChange={date => {
-                                if(date !== null) changeDate(date)
+                                if (date !== null) changeDate(date)
                             }}
                             format="yyyy"
                             disableFuture={true}
                             disableToolbar={true}
-                        />      
+                        />
                         <DatePicker
-                            onOpen={() => dispatch({type: "FOCUS", value: ""})}
+                            onOpen={() => dispatch({ type: "FOCUS", value: "" })}
                             views={["month"]}
                             label="Month"
-                            value={new Date(formData.date)}
+                            value={new Date(formData.birthday)}
                             onChange={date => {
-                                if(date !== null) changeDate(date)
+                                if (date !== null) changeDate(date)
                             }}
                             format="MMMM"
                             disableFuture={true}
                             disableToolbar={true}
-                        />     
-                         <DatePicker
-                            onOpen={() => dispatch({type: "FOCUS", value: ""})}
+                        />
+                        <DatePicker
+                            onOpen={() => dispatch({ type: "FOCUS", value: "" })}
                             views={["date"]}
                             label="Day"
-                            value={new Date(formData.date)}
+                            value={new Date(formData.birthday)}
                             onChange={date => {
-                                if(date !== null) changeDate(date)
+                                if (date !== null) changeDate(date)
                             }}
                             format="dd"
                             disableFuture={true}
@@ -111,39 +205,69 @@ export const CreateAccount: React.FC = () => {
                 </div>
                 <FormInput
                     inputChange={dispatch}
-                    label="firstName"
+                    inputKey="firstName"
+                    label="first name"
                     inputType="text"
                     currentValue={formData.firstName}
-                    selectedLabel={formData.focusedInput}
+                    selectedKey={formData.focusedInput}
+                    required={true}
                 />
                 <FormInput
                     inputChange={dispatch}
-                    label="lastName"
+                    inputKey="lastName"
+                    label="last name"
                     inputType="text"
                     currentValue={formData.lastName}
-                    selectedLabel={formData.focusedInput}
+                    selectedKey={formData.focusedInput}
+                    required={true}
                 />
                 <FormInput
                     inputChange={dispatch}
-                    label="email" inputType="text"
+                    label="email"
+                    inputKey="email"
+                    inputType="text"
                     currentValue={formData.email}
-                    selectedLabel={formData.focusedInput}
+                    selectedKey={formData.focusedInput}
+                    required={!checkMinorStatus(formData.birthday)}
+                    validation={emailValidation}
                 />
+                {checkMinorStatus(formData.birthday) &&
+                    <FormInput
+                        inputChange={dispatch}
+                        label="guardian email"
+                        inputKey="guardianEmail"
+                        inputType="text"
+                        currentValue={formData.guardianEmail}
+                        selectedKey={formData.focusedInput}
+                        required={true}
+                        validation={guardianValidation}
+                    />
+                }
                 <FormInput
-                    inputChange={dispatch}
+                    inputChange={phoneNumberDispatch}
                     label="phone"
-                    inputType="phone"
+                    inputKey="phone"
+                    inputType="tel"
                     currentValue={formData.phone}
-                    selectedLabel={formData.focusedInput}
+                    selectedKey={formData.focusedInput}
+                    required={true}
+                    validation={phoneValidation}
                 />
                 <FormInput
                     inputChange={dispatch}
                     label="password"
+                    inputKey="password"
                     inputType="password"
                     currentValue={formData.password}
-                    selectedLabel={formData.focusedInput}
+                    selectedKey={formData.focusedInput}
+                    required={true}
                 />
-                <input className={styles.submit} type="submit" value="Create Account" />
+                <input
+                    className={isDisabled ? `${styles.submit} ${styles.disabled}` : `${styles.submit}`}
+                    type="submit"
+                    value="Create Account"
+                    disabled={isDisabled}
+                />
             </form>
         </section>
     );
