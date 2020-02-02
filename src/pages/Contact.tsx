c1d3417ba776122a6048bf8dc7bfdb207e045136
Eric Chavarria <eachavarria@bsu.edu>
2020-02-02 15:33:07 -0500

    Use the FormInput Component

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
 import whitneyAbout from '../assets/images/whitneyAbout.jpg';
 import styles from '../styles/Contact.module.scss';
+import textareaStyles from '../styles/ContactTextArea.module.scss';
+import { FormInput } from '../components/FormInput';
+import { InputChangeParameters } from  "../types/FormInputTypes";
+
 
 
 export const Contact : React.FC= () => {
@@ -8,6 +12,8 @@ export const Contact : React.FC= () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [selectedInputKey, setSelectedInputKey] = useState('')
+    //const [contactFail, setContactFail] = useState(false);
     const [contactFormSubmitted, setContactFormSubmitted] = useState(false);
 
     const submitContactForm = (e : React.FormEvent) => {
@@ -16,25 +22,62 @@ export const Contact : React.FC= () => {
         setContactFormSubmitted(true);
     }
 
+    const handleInputChange = (action: InputChangeParameters) => {
+        if(action.type === "UPDATEFORM"){
+            if(action.formDataKey === "name") {
+                setName(action.value);
+            } else if(action.formDataKey === "email") {
+                setEmail(action.value);
+            }else if(action.formDataKey === "message"){
+                setMessage(action.value);
+            }
+        } else if(action.type === "FOCUS"){
+            setSelectedInputKey(action.value);
+        }
+    }
+
     return (
         <section>
             <img className={styles.contact_image} src={whitneyAbout} alt="Whitney Twigg's Photo"/>
             <h1 className={styles.heading}>Contact Whitney</h1>
             <p className={styles.information}>I'd love to hear from you! You can email me at twiggmusicstudio@gmail.com or through the form below. There's no such thing as a silly question.</p>
             <form className={contactFormSubmitted ? styles.contact_form_submitted : styles.contact_form} onSubmit={submitContactForm}>
-                <div className={styles.name_field}>
-                    <label className={styles.contact_labels} htmlFor="name"><b>First and Last Name</b></label>
-                    <input name="name" type="text" required onChange={e => setName(e.target.value)}/>
-                </div>
-                <div className={styles.email_field}>
-                    <label className={styles.contact_labels} htmlFor="email"><b>Email</b></label>
-                    <input name="email" type="text" required onChange={e => setEmail(e.target.value)}/>
-                </div>
-                <div className={styles.message_field}>
-                    <label className={styles.contact_labels} htmlFor="message"><b>Message</b></label>
-                    <textarea name="message" value={message} required onChange={e => setMessage(e.target.value)}/>
-                </div>
-                {message}
+                <FormInput
+                    inputChange={handleInputChange}
+                    inputKey="name"
+                    label="Name"
+                    inputType="text"
+                    currentValue={name}
+                    selectedKey={selectedInputKey}
+                    required={true}
+                    styles={styles}
+                >
+                <input/>
+                </FormInput>
+                <FormInput
+                    inputChange={handleInputChange}
+                    inputKey="email"
+                    label="Email"
+                    inputType="text"
+                    currentValue={email}
+                    selectedKey={selectedInputKey}
+                    required={true}
+                    styles={styles}
+                >
+                <input/>
+                </FormInput>
+                <FormInput
+                    inputChange={handleInputChange}
+                    inputKey="message"
+                    label="Message"
+                    inputType="text"
+                    currentValue={message}
+                    selectedKey={selectedInputKey}
+                    required={true}
+                    styles={textareaStyles}
+                >
+                <textarea/>
+                </FormInput>
                 <input className={styles.send} type="submit" value="Send"/>
             </form>
             <h1 className={styles.submitted_form_text} style={{"display":(contactFormSubmitted ? "block" : "none")}}>Your contact form has been submitted. Whitney will be in contact with you soon.</h1>
