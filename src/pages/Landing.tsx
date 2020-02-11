c225703081d84e4d4e0da20873b918dfe418b737
Joe Overton <joeoverton2697@gmail.com>
2020-02-11 14:51:30 -0500

    Implementing feedback

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,36 +1,24 @@
 import React from 'react';
 import whitneyAbout from '../assets/images/whitneyAbout.jpg';
 import styles from '../styles/Landing.module.scss';
-import {useHistory} from "react-router-dom";
 
 export const Landing : React.FC= () => {
 
-    let history = useHistory();
-
-    const learnMore = () => {
-        history.push("/About");
-    }
-
-    const enrollClick = () => {
-        history.push("/CreateAccount");
-    }
-
     return (
-        <section>
-            {/*TODO header goes here*/}
+        <section className={styles.landing}>
             <section className={styles.background}>
                 <div className={styles.headerText}>
                     <h1>WHITNEY TWIGG</h1>
                     <h2>Violin, Viola, Voice Instructor</h2>
-                    <button onClick={enrollClick}>Enroll Today!</button>
+                    <a href={"/CreateAccount"}>Enroll Today!</a>
                 </div>
             </section>
             <div className={styles.content}>
                 <p>text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
-                <button onClick={learnMore}>Learn More</button><br/>
+                <a href={"/About"}>Learn More</a><br/>
                 <img className={styles.picture} src={whitneyAbout}/>
                 <p>text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
-                <button onClick={enrollClick}>Enroll Now!</button>
+                <a href={"/CreateAccount"}>Enroll Now!</a>
             </div>
         </section>
     );
